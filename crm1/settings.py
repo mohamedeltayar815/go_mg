1178afb73e15d452a5dd92dbadf954431a3e7a6a
mohamedeltayar815 <77938311+mohamedeltayar815@users.noreply.github.com>
2021-07-09 23:08:59 +0200

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/images/'
